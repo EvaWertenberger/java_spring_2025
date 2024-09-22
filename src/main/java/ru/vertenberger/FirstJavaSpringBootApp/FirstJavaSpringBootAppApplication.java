f94c6003690f92ad8c976f0f07a15601d33b9f94
eva <evajjikk@gmail.com>
2024-09-22 21:18:04 +0500

    initial commit

---
+++ src/main/java/ru/vertenberger/FirstJavaSpringBootApp/FirstJavaSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package ru.vertenberger.FirstJavaSpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJavaSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstJavaSpringBootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vertenberger/FirstJavaSpringBootApp/FirstJavaSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vertenberger.FirstJavaSpringBootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJavaSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
